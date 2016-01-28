4961c1372e35d376bd4b81c14dc611072170399a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2016-01-28 18:06:17 +0900

    20160128 p5.jsさわった

---
+++ 20160128/sketch.js
@@ -0,0 +1,32 @@
+var circle;
+var speed = 30;
+var radius = 100;
+
+// 初期化でcallされる
+function setup() {
+  createCanvas(500, 500);
+  circle = new MyCircle();
+}
+
+// 毎フレームcallされる
+function draw() {
+  background(255);
+  circle.move();
+}
+
+function MyCircle() {
+  this.x = width/2;
+  this.y = height/2;
+  this.degree = 0;
+  this.rad = 0;
+  this.radius = radius;
+
+  this.move = function() {
+    this.degree += 3;
+    this.rad = this.degree * Math.PI / 180;
+    this.x = width/2 + this.radius * Math.cos(this.rad);
+    this.y = height/2 + this.radius * Math.sin(this.rad);
+
+    ellipse(this.x, this.y, 30, 30);
+  }
+}
